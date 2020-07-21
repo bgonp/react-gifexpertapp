caf97eb8b7c2acd97798a9715885055c640e6528
Borja Gonzalez <borja@bgon.es>
2020-07-21 13:27:09 +0200

    First commit

---
+++ docs/precache-manifest.1c54668fdd63a63a26ba7457ac8d109e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f51d456a81d689e6688261fe42a597b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1fcb8272faddaa40b2",
+    "url": "/static/css/main.e1b0403b.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1fcb8272faddaa40b2",
+    "url": "/static/js/main.d9de348f.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
